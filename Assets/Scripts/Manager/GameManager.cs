e30accfc9e5007ad63b9ecd16683818ab6a7fe3e
Vyles <Vyles17@gmail.com>
2026-03-18 18:12:47 +0100

    Basi UI + Game MNG + InputMap
    Aggiunta una base di InGame UI a cui andranno poi sostiuite le sprite con quelle dei 2D. Aggiunto il Game Manager script, contenente attualmente gli stati del gioco. Aggiunto InputMap per inserire i vari input del player (attualmente contiene solo quello per il tasto Pausa e il "metodo Poteri")

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,16 +1,128 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum GameStatus
+{
+    Running,
+    Paused,
+    ETmode //status per quando stiamo usando i poteri
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Script per settare gli stati del gioco
+
+    //Singleton del GM
+    public static GameManager Instance;
+
+    //bools per gli stati di gioco
+    private bool isPaused = false;
+    private bool isETing = false;
+
+    //contatore dei pezzi di bici trovati (Win condition)
+    public int bikePieces = 5; //pezzi da trovare
+    public int bikePiecesFound = 0; //i pezzi trovati
+
+    //Input map per gestire i comandi per la UI
+    private InputMap inputMap;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        inputMap = new InputMap();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        inputMap.Enable();
+        inputMap.GameStatus.Pause.performed += Pause;
+        inputMap.GameStatus.PowerMode.performed += ETMode;
+    }
+
+    void OnDisable()
+    {
+        inputMap.Disable();
+        inputMap.GameStatus.Pause.performed -= Pause;
+        inputMap.GameStatus.PowerMode.performed -= ETMode;
+    }
+
+    public void SetGameStatus(GameStatus status)
+    {
+        //metodo per settare lo stato di gioco, switchandolo da uno all'altro
+        switch (status)
+        {
+            case GameStatus.Running:
+                Time.timeScale = 1;
+                break;
+
+            case GameStatus.Paused:
+                Time.timeScale = 0;
+                break;
+
+            case GameStatus.ETmode:
+                Time.timeScale = 0;
+                break;
+        }
+    }
+
+    public void Pause(InputAction.CallbackContext context)
+    {
+        isPaused = !isPaused;
+
+        //avvio/tolgo il menu di pausa in base allo stato del gioco
+        if (isPaused)
+        {
+            SetGameStatus(GameStatus.Paused);
+            UIManager.Instance.PauseUI();
+        }
+
+        else
+        {
+            SetGameStatus(GameStatus.Running);
+            UIManager.Instance.PauseUI();
+        }
+    }
+
+    //non badare a questo doppio metodo Loris, non posso usare l'altro per il bottone del Telefono rip
+    public void PausePhone()
+    {
+        isPaused = !isPaused;
+
+        //avvio/tolgo il menu di pausa in base allo stato del gioco
+        if (isPaused)
+        {
+            SetGameStatus(GameStatus.Paused);
+            UIManager.Instance.PauseUI();
+        }
+
+        else
+        {
+            SetGameStatus(GameStatus.Running);
+            UIManager.Instance.PauseUI();
+        }
+    }
+
+    public void ETMode(InputAction.CallbackContext context)
+    {
+        isETing = !isETing;
+
+        //in base se siamo alla modalità poteri, possiamo usarli
+        if (isETing)
+        {
+            SetGameStatus(GameStatus.ETmode);
+            //aggiungere qui i poteri
+        }
+        else
+        {
+            SetGameStatus(GameStatus.Running);
+            //qui disabilitare i poteri
+        }
     }
 }
--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Script per settare la UI
+
+    //Singleton del UIM
+    public static UIManager Instance;
+
+    //i vari menu
+    public GameObject pauseMenuUI;
+
+    //gli oggetti UI in HUD
+    [SerializeField] Image staminaFill, powersFill;
+    [SerializeField] Image staminaIcon;
+    [SerializeField] Image bike_ruota, bike_pedale, bike_manubrio, bike_cestello, bike_sellino;
+    [SerializeField] Button phoneButton;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        //all'inizio il menu è disattivato
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void PauseUI()
+    {
+        //se il menu è attivo
+        if (pauseMenuUI.activeSelf)
+        {
+            //lo disttivo
+            pauseMenuUI.SetActive(false);
+        }
+
+        else
+        {
+            //e viceversa
+            pauseMenuUI.SetActive(true);
+        }
     }
 }
