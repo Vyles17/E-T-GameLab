dccecbec2ccfb12dcd0f8896fe085b919ab6943b
Vyles <Vyles17@gmail.com>
2026-03-19 02:39:27 +0100

    Inizio EnemyMovement
    Creato lo script per far muovere l'enemy Agent in giro per il NavMesh Ground. Creata anche una prima versione della cattura del player, con il suo consecutivo ingabbiamento

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,16 +1,77 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //script per il movimento dell'enemy
+
+    NavMeshAgent enemyAgent; //il nostro nemico
+    public Transform targetPlayer; // il player
+    public Transform cage; // la nostra gabbia
+    private bool isCaged; //lo stato del player in cui non può muoversi dalla gabbia
+
+    public int triggerRadius = 30; //il raggio dell'overlap sphere entro il quale l'enemy si accorge del player
+    [SerializeField] private LayerMask playerLayermask; //il layer del player
+
+    void Awake()
     {
-        
+        enemyAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //se il player finisce nel raggio dell'enemy (e non è già in gabbia)
+        if (!isCaged && Physics.OverlapSphere(transform.position, triggerRadius, playerLayermask).Length > 0)
+        {
+            //l'enemy lo insegue
+            GetET();
+
+            //se raggiunge il player
+            if (!enemyAgent.pathPending && enemyAgent.remainingDistance <= enemyAgent.stoppingDistance)
+            {
+                //porta il player nella gabbia
+                CageET();
+            }
+        }
+
+        //altrimenti se ne va a zonzo arrrandom sperando di beccare il player
+        else
+        {
+            SearchForET();
+        }
+    }
+
+    void SearchForET()
+    {
+
+    }
+
+    void GetET()
+    {
+        enemyAgent.SetDestination(targetPlayer.position); //lo insegue
+    }
+
+    //poi magari trasferisco le trappole dei nemici in uno script a parte
+    void CageET()
+    {
+        //gettiamo la posizione della gabbia, ma manteniamo la y del player
+        Vector3 cagedETposition = new Vector3(cage.position.x, targetPlayer.position.y, cage.position.z);
+
+        //ci gettiamo il suo rigidbody per "freezarlo" nella gabbia
+        Rigidbody playerRB = targetPlayer.GetComponent<Rigidbody>();
+
+        //e lo spostiamo e ingabbiamo
+        playerRB.position = cagedETposition; 
+        //playerRB.constraints = RigidbodyConstraints.FreezeAll;
+
+        //settiamo lo status
+        isCaged = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, triggerRadius);
     }
 }
